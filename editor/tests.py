a5f758ca2c72da106708f71f3c671618255558e1
bunseokbot <bunseokbot@gmail.com>
2018-08-04 23:25:16 +0900

    Remove pytest and change into default code

--- editor/tests.py
+++ editor/tests.py
@@ -1,3 +1,46 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
-# Create your tests here.
+from editor.models import Memo
+
+
+class MemoClientTest(TestCase):
+    """Memo client functional test."""
+
+    def setUp(self):
+        self.client = Client()
+
+    def test_show_all_memo(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_add_new_memo(self):
+        response = self.client.post('/editor/', {'title': 'test', 'content': '#test'})
+        self.assertEqual(response.status_code, 302)
+
+    def test_access_invalid_memo(self):
+        response = self.client.get('/editor/1/')
+        self.assertEqual(response.status_code, 404)
+
+
+class MemoModelTests(TestCase):
+    """Memo model testcases."""
+
+    def setUp(self):
+        self.memo = Memo(title='basic memo')
+        self.memo.save()
+
+    def test_add_new_memo(self):
+        """Test add new memo to database."""
+        memo = Memo(title='memo test', content='#test')
+        memo.save()
+
+        self.assertEqual(memo.title, 'memo test')
+        self.assertEqual(memo.content, '#test')
+
+    def test_edit_memo(self):
+        self.assertEqual(self.memo.title, 'basic memo')
+
+        self.memo.title = 'basic edit memo'
+        self.memo.save()
+
+        self.assertEqual(self.memo.title, 'basic edit memo')
